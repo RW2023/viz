185a966f1d5d6e386c2685f0dd179dbe0e21083b
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-02 11:07:23 -0500

    Update hero overlay opacity and change button text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,12 @@ export default function Home() {
           'url(/hero.webp)',
       }}
     >
-      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-overlay bg-opacity-40"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
           <Heading title='Welcome to the My Viz app' />
           <SubHeading title='I Create, share, and explore visualizations' />
-          <button type='submit' className="btn btn-primary">Get Started</button>
-        </div>
+          <button type='submit' className="btn bg-button text-buttonText hover:bg-buttonText hover:text-button hover:border-button">Explore Data</button>        </div>
       </div>
     </div>
   );
