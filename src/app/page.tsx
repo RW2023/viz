1b94977fd9dc1f51579dd3af16a712f7ea071d9e
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-02 15:44:47 -0500

    Update hero image and content in Home component. Change prop to take jsx element. update interface on GridBox component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
-import Heading from '@/components/ui/Heading';
 import SubHeading from '@/components/ui/SubHeading';
 import GridBox from '@/components/ui/GridBox';
 import LightHeading from '@/components/ui/LightHeading';
 import LightSubHeading from '@/components/ui/LightSubHeading';
-
 
 export default function Home() {
   return (
     <div
       className="hero min-h-screen"
       style={{
-        backgroundImage: 'url(/hero.webp)',
+        backgroundImage: 'url(/photo1Hero.jpg)',
       }}
     >
       <div className="hero-overlay bg-opacity-40"></div>
@@ -26,12 +24,31 @@ export default function Home() {
           </button>
           <div className="w-full">
             <GridBox
-              heading1="Stories"
-              heading2="Insights"
-              content1="Discover the stories locked in Data"
+              heading1="Unlock the Power of Stories Hidden in Data"
+              heading2="Gaining Insights Through the Lens of Data Science"
+              content1=<div>
+                <p>
+                  Data has a story to tell. It is up to us to discover the tales
+                  hidden within. My Viz app is a platform for creating, sharing,
+                  and exploring data stories.
+                </p>
+              </div>
               content2="Through data science we will discover the tales data has to tell"
               buttonText="Continue"
             />
+            <div>
+              <p>Much love for the hero</p>{' '}
+              <span>
+                courtesy{' '}
+                <a href="https://unsplash.com/@dawson2406?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+                  Stephen Dawson
+                </a>{' '}
+                on{' '}
+                <a href="https://unsplash.com/photos/turned-on-monitoring-screen-qwtCeJ5cLYs?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+                  Unsplash
+                </a>
+              </span>
+            </div>
           </div>
         </div>
       </div>
--- src/components/ui/GridBox.tsx
+++ src/components/ui/GridBox.tsx
@@ -1,12 +1,13 @@
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
 import SubHeading from './SubHeading';
+import LightSubHeading from './LightSubHeading';
 
 interface Props {
   heading1: string;
-  content1: string;
+  content1: ReactNode;
   buttonText: string;
   heading2: string;
-  content2: string;
+  content2: ReactNode;
 }
 
 const GridBox: FC<Props> = ({
@@ -14,19 +15,18 @@ const GridBox: FC<Props> = ({
   heading2,
   content1,
   content2,
-  buttonText,
 }): JSX.Element => {
   return (
-    <div className="grid md:grid-cols-2 gap-4 mt-4 bg-highlight text-secondary border border-1 p-2 rounded-md text-3xl">
-      <div className="bg-background p-4 rounded shadow card-compact m-1">
-        <h3 className="text-headline">{heading1}</h3>{' '}
+    <div className="grid md:grid-cols-2 gap-4 mt-4 bg-headline text-secondary border border-1 p-2 rounded-md text-xl">
+      <div className="bg-highlight p-4 rounded shadow card-compact m-1">
+        <LightSubHeading title={heading1} />
         {/* Assuming SubHeading renders an <h3> */}
         <div className="card-body">
           <p className="text-paragraph text-lg ">{content1}</p>
         </div>
       </div>
-      <div className="bg-background p-4 rounded shadow card-compact m-1">
-        <h3 className="text-headline">{heading2}</h3>{' '}
+      <div className="bg-highlight p-4 rounded shadow card-compact m-1">
+       <LightSubHeading title={heading2} />
         {/* Adjust according to SubHeading's render */}
         <div className="card-body">
           <p className="text-paragraph text-lg">{content2}</p>
