d954ecef3b0445f275c188e2d22952e89671883c
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-02 16:13:00 -0500

    Remove unused buttonText prop in GridBox component. update comments. add content variables

--- src/components/ui/GridBox.tsx
+++ src/components/ui/GridBox.tsx
@@ -5,7 +5,6 @@ import LightSubHeading from './LightSubHeading';
 interface Props {
   heading1: string;
   content1: ReactNode;
-  buttonText: string;
   heading2: string;
   content2: ReactNode;
 }
@@ -19,17 +18,16 @@ const GridBox: FC<Props> = ({
   return (
     <div className="grid md:grid-cols-2 gap-4 mt-4 bg-headline text-secondary border border-1 p-2 rounded-md text-xl">
       <div className="bg-highlight p-4 rounded shadow card-compact m-1">
+        {/* 'Light' components have dark text outline applied to make them stand out against light backgrounds */}
         <LightSubHeading title={heading1} />
-        {/* Assuming SubHeading renders an <h3> */}
         <div className="card-body">
-          <p className="text-paragraph text-lg ">{content1}</p>
+          <div className="text-xl">{content1}</div>
         </div>
       </div>
       <div className="bg-highlight p-4 rounded shadow card-compact m-1">
-       <LightSubHeading title={heading2} />
-        {/* Adjust according to SubHeading's render */}
+        <LightSubHeading title={heading2} />
         <div className="card-body">
-          <p className="text-paragraph text-lg">{content2}</p>
+          <div className="text-xl">{content2}</div>
         </div>
       </div>
     </div>
