03af88cb5959e3a82dcf7d6977512557e4e114d2
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-02 20:24:23 -0500

    Update ArticleImage component styling and image dimensions

--- src/components/ui/ArticleImage.tsx
+++ src/components/ui/ArticleImage.tsx
@@ -8,12 +8,12 @@ interface Props {
 
 const ArticleImage: FC<Props> = ({ src, alt }): JSX.Element => {
   return (
-    <div className="rounded bg-background p-2 shadow-lg border border-1 border-headline m-4">
+    <div className="rounded bg-background p-2 shadow-lg border border-1 border-headline m-2">
       <Image
         src={src}
         alt={alt}
-        width={400}
-        height={400}
+        width={350}
+        height={350}
       />
     </div>
   );
