35ae9db9ffdb386bf63b8e3409195aa3007bc6eb
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-02 20:24:10 -0500

    Refactor content in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const content1 = (
   <div className="flex flex-1 flex-col justify-center items-center text-left">
 <ArticleImage src={'/chart2.jpg'} alt={'chart'} />
     <p>
-      Here I will explore the world of data science and the stories that can be told through data.
+      Here I will explore various data sets and the insights that can be gained
     </p>
 
   </div>
@@ -20,9 +20,7 @@ const content2 = (
   <div className="flex flex-1 flex-col justify-center items-center text-left">
     <ArticleImage src={'/chart.jpg'} alt={'chart'} />
     <p>
-      Data science is a powerful tool for gaining insights. It is through the
-      lens of data science that we can gain a deeper understanding of the world
-      around us.
+      Data science is a powerful tool for gaining insights. I will analyze the data and tell the story
     </p>
   </div>
 );
