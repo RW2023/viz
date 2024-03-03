a657024a87b48b70aa115f8fcb24c68c4b0c0411
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-03 00:57:26 -0500

    Update article images in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,19 @@ import LightHeading from '@/components/ui/LightHeading';
 import LightSubHeading from '@/components/ui/LightSubHeading';
 import Attribution from '@/components/ui/Attribution';
 import ArticleImage from '@/components/ui/ArticleImage';
-import SubHeading from '@/components/ui/SubHeading';
 
 const content1 = (
   <div className="flex flex-1 flex-col justify-center items-center text-left">
-<ArticleImage src={'/chart2.jpg'} alt={'chart'} />
+    <ArticleImage src={'/csv.jpg'} alt={'chart'} />
     <p>
       Here I will explore various data sets and the insights that can be gained
     </p>
-
   </div>
 );
 
 const content2 = (
   <div className="flex flex-1 flex-col justify-center items-center text-left">
-    <ArticleImage src={'/chart.jpg'} alt={'chart'} />
+    <ArticleImage src={'/present.jpg'} alt={'chart'} />
     <p>
       Data science is a powerful tool for gaining insights. I will analyze the data and tell the story
     </p>
@@ -27,6 +25,7 @@ const content2 = (
 
 
 export default function Home() {
+  
   return (
     <div
       className="hero min-h-screen"
@@ -46,14 +45,6 @@ export default function Home() {
               buttonText1='Explore Data'
               buttonText2='Explore Stories'
             />
-            {/* <GridBox 
-            heading1='Data Sets'
-            heading2='Data Stories'
-            content1={content1}
-            content2={content2}
-            buttonText1='Explore Data Sets'
-            buttonText2='Explore Data Stories'
-            /> */}
             <Attribution
               link="https://unsplash.com/@dawson2406?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
               artist="Stephen Dawson"
