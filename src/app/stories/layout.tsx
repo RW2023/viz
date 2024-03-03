557687108f1d08f0b266b6e61bb571dc0be08f68
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-03 08:47:55 -0500

    Add layout component for Stories page

---
+++ src/app/stories/layout.tsx
@@ -0,0 +1,27 @@
+import Footer from '@/components/ui/Footer';
+import Navbar from '@/components/ui/Navbar';
+import type { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+  title: 'Stories',
+  description: 'Data Visualizations',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" data-theme="business">
+      <body>
+        <main className='flex flex-col justify-center items-center'>
+          <Navbar />
+          {children}
+          </main>
+      </body>
+      <Footer />
+    </html>
+  );
+}
