bfcedd0f37eb61335c710a72dd1f57a2b3560081
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-03 01:51:51 -0500

    Refactor page component and update hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,8 @@
 //src/app/page.tsx
-import GridBox from '@/components/ui/GridBox';
-import LightHeading from '@/components/ui/LightHeading';
-import LightSubHeading from '@/components/ui/LightSubHeading';
-import Attribution from '@/components/ui/Attribution';
-import ArticleImage from '@/components/ui/ArticleImage';
 
-const content1 = (
-  <div className="flex flex-1 flex-col justify-center items-center text-left">
-    <ArticleImage src={'/csv.jpg'} alt={'chart'} />
-    <p>
-      Here I will explore various data sets and the insights that can be gained
-    </p>
-  </div>
-);
-
-const content2 = (
-  <div className="flex flex-1 flex-col justify-center items-center text-left">
-    <ArticleImage src={'/present.jpg'} alt={'chart'} />
-    <p>
-      Data science is a powerful tool for gaining insights. I will analyze the data and tell the story
-    </p>
-  </div>
-);
-
+import LightHeading from "@/components/ui/LightHeading";
+import LightSubheading from "@/components/ui/LightSubHeading"
+import Link from "next/link";
 
 export default function Home() {
   
@@ -30,27 +10,21 @@ export default function Home() {
     <div
       className="hero min-h-screen"
       style={{
-        backgroundImage: 'url(/photo1Hero.jpg)',
+        backgroundImage: 'url(/data.webp)',
       }}
     >
       <div className="hero-overlay bg-opacity-40"></div>
       <div className="hero-content text-center text-neutral-content">
-        <div className="text-headline">
-          <div className="w-full">
-            <GridBox
-              heading1="The Data"
-              heading2="The story"
-              content1={content1}
-              content2={content2}
-              buttonText1='Explore Data'
-              buttonText2='Explore Stories'
-            />
-            <Attribution
-              link="https://unsplash.com/@dawson2406?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
-              artist="Stephen Dawson"
-              artistAttribution="https://unsplash.com/photos/turned-on-monitoring-screen-qwtCeJ5cLYs?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
-            />
+        <div className="max-w-md bg-base-300 border border-1 rounded-md w-full m-5 p-3 flex flex-1 flex-col justify-center items-center">
+          <div className="bg-highlight border border-1 rounded w-full m-5 p-3 flex flex-1 flex-col justify-center items-center">
+            <LightHeading title="Data Science" />
+            <LightSubheading title="Analysis and Machine Learning" />
           </div>
+          <Link href="/landing">
+            <button className="btn btn-primary" type="submit">
+              Explore
+            </button>
+          </Link>
         </div>
       </div>
     </div>
