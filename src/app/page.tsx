f9214ce92a37266ffd94610cb2c3cc507d801c94
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-03 04:58:19 -0500

    Add GridBox component and update content in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,40 @@
 //src/app/page.tsx
 
+import GridBox from "@/components/ui/GridBox";
 import LightHeading from "@/components/ui/LightHeading";
 import LightSubheading from "@/components/ui/LightSubHeading"
 import Link from "next/link";
+import ArticleImage from "@/components/ui/ArticleImage";
 
 export default function Home() {
+  const content1 = (
+    <div className="flex flex-1 flex-col justify-center items-center text-left">
+      <ArticleImage src={'/csv.jpg'} alt={'data'} />
+      <p>
+        Here is where I will list the data sets i am working with and any
+        relevant information about them.
+      </p>
+      <br />
+      <p>
+        Real world data isn&apos;t neat and tidy. I have come to understand the
+        importance of data cleaning and preprocessing. I will share my
+        experiences and the tools I use to clean and preprocess data.
+      </p>
+    </div>
+  );
+
+    const content2 = (
+      <div className="flex flex-1 flex-col justify-center items-center text-left">
+        <ArticleImage src={'/present.jpg'} alt={'presentation'} />
+        <p>
+          Here I will explore various data sets and the insights that can be
+          gained
+        </p>
+      </div>
+    );
   
   return (
+    <>
     <div
       className="hero min-h-screen"
       style={{
@@ -26,7 +54,19 @@ export default function Home() {
             </button>
           </Link>
         </div>
+        <div></div>
       </div>
     </div>
+    <div className="flex flex-1 flex-col justify-center items-center w-3/4">
+        <GridBox
+        heading1="Data"
+        content1={content1}
+        buttonText1="Explore Data"
+        heading2="The story"
+        content2={content2}
+        buttonText2="Explore Stories"
+      />
+    </div>
+    </>
   );
 }
