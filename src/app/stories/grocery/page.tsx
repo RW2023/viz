556c748a0950ae673bfa8501765e9eb5118c1e26
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-04 03:24:49 -0500

    Add grocery page component

---
+++ src/app/stories/grocery/page.tsx
@@ -0,0 +1,10 @@
+import Heading from '@/components/ui/Heading';
+import { FC } from 'react';
+
+interface Props {}
+
+const page: FC<Props> = (props): JSX.Element => {
+  return <Heading title="Grocery Inventory" />;
+};
+
+export default page;
