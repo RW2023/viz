11ce76f38c6878d5ab9389bd5b0b72aa51a032d8
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-04 04:34:52 -0500

    Add Tableau component to Grocery page

--- src/app/stories/grocery/page.tsx
+++ src/app/stories/grocery/page.tsx
@@ -1,10 +1,16 @@
+import Tableau from '@/components/Tableau';
 import Heading from '@/components/ui/Heading';
 import { FC } from 'react';
 
 interface Props {}
 
 const page: FC<Props> = (props): JSX.Element => {
-  return <Heading title="Grocery Inventory" />;
+  return (
+  <div className='w-full h-auto'>
+      <Heading title="Grocery Inventory" />  
+        <Tableau />
+  </div>
+  );
 };
 
 export default page;
