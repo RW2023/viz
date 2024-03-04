c1dfdc2b5a1b1547fb1779279e750e52037b1e47
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-03 19:02:37 -0500

    Update import path for Navbar component

--- src/app/stories/layout.tsx
+++ src/app/stories/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from '@/components/ui/Footer';
-import Navbar from '@/components/ui/Navbar';
+import Navbar from './Navbar';
 import type { Metadata } from 'next';
+
 
 
 export const metadata: Metadata = {
